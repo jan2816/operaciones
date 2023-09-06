bf5300c33022fb15df997a366c8609788be664a7
jan2816 <jan99lauch@gmail.com>
2023-09-06 18:20:28 -0500

    Agregar archivos de proyecto.

---
+++ operaciones.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operaciones
+{
+    public class operaciones
+    {
+        int j = 0;
+        int i = 0;
+        public static long suma(long j, long i)
+        {
+
+            return j + i;
+        }
+
+        public static long resta(long j, long i)
+        {
+
+            return j - i;
+        }
+
+        public static long multiplicacion(long j, long i)
+        {
+
+            return j * i;
+        }
+
+        public static long division(long j, long i)
+        {
+
+            return j / i;
+        }
+
+    }
+}
